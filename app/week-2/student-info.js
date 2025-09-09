7573fbb56da988abd1b874e692ac2987957b6718
Mark-Olidan <markolidan@yahoo.com>
2025-09-09 14:02:03 -0600

    Started Week 2 Assignment
    Cleaned up the homepage and created a new page and component

---
+++ app/week-2/student-info.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+
+
+export default function StudentInfo(){
+    return(
+        <main>
+            <h1>Student Information</h1>
+            <p>Name: Mark Angelo Olidan</p>
+            <Link href="https://www.https://github.com/Mark-Olidan/cprg306-assignments.ca">Github Repository</Link>
+        </main>
+    )
+}
