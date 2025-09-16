569470757f1669bac38ddb48edbb77aa7e4da983
Mark-Olidan <markolidan@yahoo.com>
2025-09-16 14:46:45 -0600

    week-3
    added the li semantics for list items

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -7,8 +7,10 @@ export default function Item({item}){
     return(
         <main>
             <div className="bg-blue-600 pt-3 pb-3 pl-2 m-3 w-1/5 mx-auto hover:bg-emerald-500">
-                <h1 className="text-2xl font-bold">{name}</h1>
-                <p>Buy {quantity} in {category}</p>
+            <ul>
+                <li className="text-2xl font-bold">{name}</li>
+                <li>Buy {quantity} in {category}</li>
+            </ul>
             </div>
         </main>
     )
