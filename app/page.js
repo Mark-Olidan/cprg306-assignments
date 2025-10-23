4d39fd3ae4a9e81ff08251865018039ccab96013
Mark Olidan <markangelo.olidan@edu.sait.ca>
2025-10-23 08:32:33 -0600

    week 7 - hooked up all the components to each other
    created a random string generator for the id

--- app/page.js
+++ app/page.js
@@ -1,15 +1,16 @@
 export default function Home(){
 
-
+  let style="text-cyan-600 underline hover:text-cyan-300";
   return (
     <main>
       <h1>CPRG306 Assignments</h1>
       <ul>
-        <li><a className="text-cyan-600 underline hover:text-cyan-300" href="./week-2">Week 2 - Student Info</a></li>
-        <li><a className="text-cyan-600 underline hover:text-cyan-300" href="./week-3">Week 3 - Shopping List</a></li>
-        <li><a className="text-cyan-600 underline hover:text-cyan-300" href="./week-4">Week 4 - Quantity Controls</a></li>
-        <li><a className="text-cyan-600 underline hover:text-cyan-300" href="./week-5">Week 5 - Controlled Components</a></li>
-        <li><a className="text-cyan-600 underline hover:text-cyan-300" href="./week-6">Week 6 - Handling Lists</a></li>
+        <li><a className={style} href="./week-2">Week 2 - Student Info</a></li>
+        <li><a className={style} href="./week-3">Week 3 - Shopping List</a></li>
+        <li><a className={style} href="./week-4">Week 4 - Quantity Controls</a></li>
+        <li><a className={style} href="./week-5">Week 5 - Controlled Components</a></li>
+        <li><a className={style} href="./week-6">Week 6 - Handling Lists</a></li>
+        <li><a className={style} href="./week-7">Week 7 - Managing State</a></li>
       </ul>
     </main>
   );
